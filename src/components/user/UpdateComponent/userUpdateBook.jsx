2833ea5018298cd46481640f3c8bc6bac3aa8584
BharatLinkerRetailer <bharatlinkerretailer@gmail.com>
2025-01-03 22:16:36 +0530

    push

--- src/components/user/UpdateComponent/userUpdateBook.jsx
+++ src/components/user/UpdateComponent/userUpdateBook.jsx
@@ -1,14 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { FaArrowLeft } from 'react-icons/fa';
-
-import { useSelector } from 'react-redux';
-
-import { IoClose } from 'react-icons/io5';
-import { CiImageOn } from "react-icons/ci";
-import Cookies from 'js-cookie';
-
-import userUploadBooks from '../../../appWrite/UserUploadRefurbished/userUploadBooks.js';
 import './userUpdateBook.css';
 
 const UploadProduct = () => {
