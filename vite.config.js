423286eba1386c71b22f6a4d9bf6efcc08652353
BharatLinkerRetailer <bharatlinkerretailer@gmail.com>
2025-01-20 00:48:15 +0530

    pushy

--- vite.config.js
+++ vite.config.js
@@ -40,10 +40,10 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     server: {
-      host: '0.0.0.0', // Allows access from your mobile device
-      port: 5173, // Port on which Vite will run
-      strictPort: true, // Ensures Vite does not use an alternative port
-      cors: true, // Enable CORS for mobile access
+      host: '0.0.0.0', 
+      port: 5173,
+      strictPort: true, 
+      cors: true, 
     },
     root: '.',
   };
