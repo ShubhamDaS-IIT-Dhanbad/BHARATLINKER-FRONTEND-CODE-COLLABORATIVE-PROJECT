4a733d9828db38d1665a3da28cef7a5fcc26ce6d
BharatLinkerRetailer <bharatlinkerretailer@gmail.com>
2025-01-30 00:06:53 +0530

    pushy

--- src/components/retailer/upload/upload.jsx
+++ src/components/retailer/upload/upload.jsx
@@ -1,39 +1,39 @@
 import React from 'react';
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 import UploadBooksForm from './userUploadProductForm.jsx';
 
 import './upload.css';
 
-const UploadProduct = ({retailerData}) => {
+const UploadProduct = ({ retailerData }) => {
     const navigate = useNavigate();
 
     return (
         <>
-           <div className='retailer-upload-header'>
-    <FaArrowLeft
-        className='retailer-upload-back-icon'
-        size={25}
-        onClick={() => navigate('/retailer')}
-        aria-label="Back to Retailer Dashboard"
-        tabIndex={0}
-    />
-    <div className='retailer-upload-header-content'>
-        <h1 className='retailer-upload-title'>
-            Upload New Product
-            <span className='retailer-upload-title-underline'></span>
-        </h1>
-        <div
-            className='retailer-upload-shopname'
-            aria-label="Current Shop"
-            tabIndex={0}
-        >
-            <span className='retailer-upload-shopname-icon'>🏪</span>
-            {retailerData?.shopName}
-        </div>
-    </div>
-</div>
-           
+            <div className='retailer-upload-header'>
+                <FaArrowLeft
+                    className='retailer-upload-back-icon'
+                    size={25}
+                    onClick={() => navigate('/retailer')}
+                    aria-label="Back to Retailer Dashboard"
+                    tabIndex={0}
+                />
+                <div className='retailer-upload-header-content'>
+                    <h1 className='retailer-upload-title'>
+                        Upload New Product
+                        <span className='retailer-upload-title-underline'></span>
+                    </h1>
+                    <div
+                        className='retailer-upload-shopname'
+                        aria-label="Current Shop"
+                        tabIndex={0}
+                    >
+                        <span className='retailer-upload-shopname-icon'>🏪</span>
+                        {retailerData?.shopName}
+                    </div>
+                </div>
+            </div>
+
 
             <UploadBooksForm retailerData={retailerData} />
 
