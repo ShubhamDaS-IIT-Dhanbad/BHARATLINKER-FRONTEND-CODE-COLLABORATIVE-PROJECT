fa8292979357338dc1cec744941ce38ec16eb70d
BharatLinkerRetailer <bharatlinkerretailer@gmail.com>
2025-02-08 23:05:25 +0530

    push to bharat linker 8-2-25

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,23 @@
 // postcss.config.mjs
 import postcssPresetEnv from "postcss-preset-env";
 import cssnano from "cssnano";
-import purgecss from "@fullhuman/postcss-purgecss";
+import purgecssModule from "@fullhuman/postcss-purgecss";
+
+const purgecss = purgecssModule.default || purgecssModule;
 
 export default {
   plugins: [
     postcssPresetEnv({
-      stage: 1,
+      stage: 1, // Enables modern CSS features like nesting
       autoprefixer: { grid: true }, // Adds vendor prefixes
     }),
+
+    // Minifies the final CSS output
     cssnano({
       preset: "default",
     }),
+
+    // Removes unused CSS from the final build
     purgecss({
       content: [
         "./src/**/*.{js,jsx,ts,tsx}",
