9f66343f4d8c23192e921e99fb7e55d28b975ecb
BharatLinkerRetailer <bharatlinkerretailer@gmail.com>
2025-02-27 11:43:37 +0530

    push to bharat linker leaf plate correction 8-2-25

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
-import React, { Suspense, lazy, useEffect, useState } from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Cookies from 'js-cookie';
 import { useDispatch, useSelector } from "react-redux";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import useUserAuth from './hooks/userAuthHook.jsx';
 import { Oval } from 'react-loader-spinner';
-import './App.css'
+import useUserAuth from './hooks/userAuthHook.jsx';
+import { fetchUserCart } from "./redux/features/user/cartSlice.jsx";
+import './App.css';
+
+// Static Imports
 import Download from './components/downloadapp.jsx';
 import HomePage from './components/homePage/home.jsx';
 import SearchPage from './components/searchPage/searchPage.jsx';
 import SearchShop from './components/searchShop/searchShop.jsx';
-const LoginPage = lazy(() => import('./components/user/login.jsx'));
 
+// Lazy Loaded Components
+const LoginPage = lazy(() => import('./components/user/login.jsx'));
 const SingleProduct = lazy(() => import('./components/singleProduct.jsx'));
 const SingleShopCard = lazy(() => import('./components/singleShop.jsx'));
 const ShopProducts = lazy(() => import('./components/shopProducts/shopProducts.jsx'));
+
 const RetailerRoutes = lazy(() => import('./components/retailer/retailerRoutes.jsx'));
-const RetailerLogin = React.lazy(() => import("./components/retailer/login.jsx"));
-const RetailerRegister = React.lazy(() => import("./components/retailer/register.jsx"));
-
-
-
+const RetailerLogin = lazy(() => import("./components/retailer/login.jsx"));
+const RetailerRegister = lazy(() => import("./components/retailer/register.jsx"));
 
 const User = lazy(() => import('./components/user/userHome.jsx'));
 const UserProfile = lazy(() => import('./components/user/userProfile.jsx'));
 const UserOrder = lazy(() => import('./components/user/order/order.jsx'));
 const UserCart = lazy(() => import('./components/user/myCart/myCart.jsx'));
-const UserOrderDetail = lazy(() => import('./components/user/orderDetail/orderDetail.jsx'));
 
-import { fetchUserCart } from "./redux/features/user/cartSlice.jsx";
+// Route Configuration
+const routes = {
+  public: [
+    { path: "/", component: HomePage, title: "Bharat Linker", desc: "Welcome to Bharat Linker, your one-stop solution for business management.", robots: "index, follow" },
+    { path: "/download", component: Download, title: "Bharat Linker", desc: "Welcome to Bharat Linker, your one-stop solution for business management.", robots: "index, follow" },
+    { path: "/login", component: LoginPage, title: "Login - Bharat Linker", desc: "Login to access your Bharat Linker account and manage your business efficiently.", robots: "noindex, nofollow" },
+    { path: "/search", component: SearchPage, title: "Search Products - Bharat Linker", desc: "Search and explore a wide range of products on Bharat Linker." },
+    { path: "/product/:productId", component: SingleProduct, title: "Product Details - Bharat Linker", desc: "View detailed information about the selected product on Bharat Linker." },
+    { path: "/shop", component: SearchShop, title: "Search Shops - Bharat Linker", desc: "Search and explore various shops on Bharat Linker." },
+    { path: "/shop/:shopId", component: SingleShopCard, title: "Shop Details - Bharat Linker", desc: "Discover detailed information about this shop on Bharat Linker." },
+    { path: "/shop/product/:shopId", component: ShopProducts, title: "Shop Products - Bharat Linker", desc: "View products from this shop on Bharat Linker." },
+    { path: "/secure/login", component: RetailerLogin, title: "Retailer Login - BharatLinker", desc: "Login to your BharatLinker Retailer account and manage your business effectively." },
+    { path: "/secure/register", component: RetailerRegister, title: "Register as a Retailer - BharatLinker", desc: "Register as a retailer on BharatLinker to expand your business opportunities." },
+    { path: "/*", component: RetailerRoutes, title: "Retailer Dashboard - Bharat Linker", desc: "Access the retailer dashboard to manage your shop, products, and orders on Bharat Linker." },
+  ],
+  private: [
+    { path: "/user", component: User, title: "User Dashboard - Bharat Linker", desc: "Access your Bharat Linker user dashboard to manage your account, orders, and more." },
+    { path: "/user/profile", component: UserProfile, title: "User Profile - Bharat Linker", desc: "View and update your Bharat Linker user profile." },
+    { path: "/user/order", component: UserOrder, title: "Orders - Bharat Linker", desc: "View and manage your orders on Bharat Linker." },
+    { path: "/user/cart", component: UserCart, title: "Cart - Bharat Linker", desc: "View and manage your cart on Bharat Linker." },
+  ]
+};
 
 function App() {
   return (
@@ -37,7 +59,8 @@ function App() {
         <Suspense fallback={
           <div className="fallback-loading">
             <Oval height={30} width={30} color="green" secondaryColor="white" ariaLabel="loading" />
-          </div>}>
+          </div>
+        }>
           <RoutesWithConditionalHeader />
         </Suspense>
       </Router>
@@ -49,252 +72,60 @@ const RoutesWithConditionalHeader = React.memo(() => {
   const { PrivateRoute } = useUserAuth();
   const { cart, isCartFetched } = useSelector((state) => state.userCart);
   const dispatch = useDispatch();
+
   useEffect(() => {
     if (!isCartFetched) {
       const userData = Cookies.get('BharatLinkerUserData');
-      let parsedUserData = null;
+      let parsedUserData;
       try {
         parsedUserData = userData ? JSON.parse(userData) : null;
       } catch (error) {
-        console.error("Not authenticated !");
+        console.error("Error parsing user data:", error);
       }
-      if (cart.length === 0 && parsedUserData?.userId && !isCartFetched) {
+      if (parsedUserData?.userId && cart.length === 0) {
         dispatch(fetchUserCart(parsedUserData.userId));
-      } 
+      }
     }
-  }, []);
+  }, [dispatch, cart.length, isCartFetched]);
 
   return (
-    <>
-      <Routes>
+    <Routes>
+      {/* Public Routes */}
+      {routes.public.map((route) => (
         <Route
-          path="/"
+          key={route.path}
+          path={route.path}
           element={
             <>
               <Helmet>
-                <title>Bharat Linker</title>
-                <meta name="description" content="Welcome to Bharat Linker, your one-stop solution for business management." />
-                <meta name="robots" content="index, follow" />
+                <title>{route.title}</title>
+                <meta name="description" content={route.desc} />
+                {route.robots && <meta name="robots" content={route.robots} />}
               </Helmet>
-              <HomePage />
+              <route.component />
             </>
           }
         />
+      ))}
+
+      {/* Private Routes */}
+      {routes.private.map((route) => (
         <Route
-          path="/download"
+          key={route.path}
+          path={route.path}
           element={
-            <>
-              <Helmet>
-                <title>Bharat Linker</title>
-                <meta name="description" content="Welcome to Bharat Linker, your one-stop solution for business management." />
-                <meta name="robots" content="index, follow" />
-              </Helmet>
-              <Download />
-            </>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <>
-              <Helmet>
-                <title>Login - Bharat Linker</title>
-                <meta name="description" content="Login to access your Bharat Linker account and manage your business efficiently." />
-                <meta name="robots" content="noindex, nofollow" />
-              </Helmet>
-              <LoginPage />
-            </>
-          }
-        />
-        {/* Search Section */}
-        <Route
-          path="/search"
-          element={
-            <>
-              <Helmet>
-                <title>Search Products - Bharat Linker</title>
-                <meta name="description" content="Search and explore a wide range of products on Bharat Linker." />
-              </Helmet >
-              <SearchPage />
-            </>
-          }
-        />
-
-        {/* Single Product */}
-        <Route
-          path="/product/:productId"
-          element={
-            <>
-              <Helmet>
-                <title>Product Details - Bharat Linker</title>
-                <meta name="description" content="View detailed information about the selected product on Bharat Linker." />
-              </Helmet>
-              <SingleProduct />
-            </>
-          }
-        />
-
-        {/* Search Shop */}
-        <Route
-          path="/shop"
-          element={
-            <>
-              <Helmet>
-                <title>Search Shops - Bharat Linker</title>
-                <meta name="description" content="Search and explore various shops on Bharat Linker." />
-              </Helmet>
-              <SearchShop />
-            </>
-          }
-        />
-
-        {/* Single Shop */}
-        <Route
-          path="/shop/:shopId"
-          element={
-            <>
-              <Helmet>
-                <title>Shop Details - Bharat Linker</title>
-                <meta name="description" content="Discover detailed information about this shop on Bharat Linker." />
-              </Helmet>
-              <SingleShopCard />
-            </>
-          }
-        />
-        <Route path="/shop/product/:shopId" element={<ShopProducts />} />
-
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <Route
-          path="/secure/login"
-          element={
-            <>
-              <Helmet>
-                <title>Retailer Login - BharatLinker</title>
-                <meta
-                  name="description"
-                  content="Login to your BharatLinker Retailer account and manage your business effectively."
-                />
-              </Helmet>
-              <RetailerLogin />
-            </>
-          }
-        />
-        <Route
-          path="/secure/register"
-          element={
-            <>
-              <Helmet>
-                <title>Register as a Retailer - BharatLinker</title>
-                <meta
-                  name="description"
-                  content="Register as a retailer on BharatLinker to expand your business opportunities."
-                />
-              </Helmet>
-              <RetailerRegister />
-            </>
-          }
-        />
-        {/* Retailer and User Routes */}
-        <Route
-          path="/*"
-          element={
-            <>
-              <Helmet>
-                <title>Retailer Dashboard - Bharat Linker</title>
-                <meta name="description" content="Access the retailer dashboard to manage your shop, products, and orders on Bharat Linker." />
-              </Helmet>
-              <RetailerRoutes />
-            </>
-          }
-        />
-
-
-
-
-
-
-
-
-
-
-
-
-        <Route
-          path="/user"
-          element=
-          {
-            <PrivateRoute >
-              <Helmet>
-                <title>User Dashboard - Bharat Linker</title>
-                <meta name="description" content="Access your Bharat Linker user dashboard to manage your account, orders, and more." />
-              </Helmet>
-              <User />
-            </PrivateRoute>
-          }
-        />
-
-        <Route
-          path="/user/profile"
-          element=
-          {
             <PrivateRoute>
               <Helmet>
-                <title>User Profile - Bharat Linker</title>
-                <meta name="description" content="View and update your Bharat Linker user profile." />
+                <title>{route.title}</title>
+                <meta name="description" content={route.desc} />
               </Helmet>
-              <UserProfile />
+              <route.component />
             </PrivateRoute>
           }
         />
-        <Route
-          path="/user/order"
-          element=
-          {
-            <PrivateRoute>
-              <Helmet>
-                <title>Orders - Bharat Linker</title>
-                <meta name="description" content="View and manage your orders on Bharat Linker." />
-              </Helmet>
-              <UserOrder />
-            </PrivateRoute>
-          }
-        />
-
-        <Route
-          path="/user/cart"
-          element=
-          {
-            <PrivateRoute>
-              <Helmet>
-                <title>Cart - Bharat Linker</title>
-                <meta name="description" content="View and manage your cart on Bharat Linker." />
-              </Helmet>
-              <UserCart />
-            </PrivateRoute>
-          }
-        />
-        
-
-      </Routes>
-    </>
-  )
+      ))}
+    </Routes>
+  );
 });
 
 export default App;
