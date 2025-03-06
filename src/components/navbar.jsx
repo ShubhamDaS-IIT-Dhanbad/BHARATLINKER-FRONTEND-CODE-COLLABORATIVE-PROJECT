c5a6c56cbf68acaed04d8e3eb11fc5f2004efc3e
BharatLinkerRetailer <bharatlinkerretailer@gmail.com>
2025-03-06 16:19:34 +0530

    oush

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -51,16 +51,21 @@ const Navbar = ({ headerTitle, shopId }) => {
         `" jewelry store "`,
         `" pet store "`
     ] : [
-        `" iPhone 14 Pro Max "`,
-        `" Samsung Galaxy S23 Mobile "`,
-        `" Apple AirPods Pro Ear Buds "`,
-        `" Sony WF-1000XM5 Wireless Ear Buds "`,
-        `" iPhone SE 2023 "`,
-        `" Google Pixel 8 Mobile "`,
-        `" Jabra Elite 8 Active Ear Buds "`,
-        `" Beats Studio Buds for iPhone "`,
-        `" OnePlus Nord 3 Mobile "`,
-        `" Bose QuietComfort Ultra Ear Buds "`
+        ` " Moong Dal " `,
+        ` " iPhone 14 Pro Max " `,
+        ` " Whole Wheat Flour (Atta) " `,
+        ` " Sugar " `,
+        ` " Turmeric Powder " `,
+        ` " Tea Leaves " `,
+        ` " Cooking Salt " `,
+        ` " Samsung Galaxy S23 Mobile " `,
+        ` " Air Conditioner " `,
+        ` " Kacchi Ghani Sarso Tel " `,
+        ` " Apple AirPods Pro Ear Buds " `,
+        ` " OnePlus Nord 3 Mobile " `,
+        ` " Basmati Rice " `,
+        ` " Besan (Gram Flour) " `
+
     ];
 
     const handleRetailerClick = useCallback(() => {
